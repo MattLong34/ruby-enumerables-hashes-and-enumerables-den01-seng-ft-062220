311249b5638c8f0faeedbe5401eac914979b643a
Matt Long <mdlong34@gmail.com>
2020-06-03 19:45:06 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,6 +4,9 @@
 #   "Amanda" => 27
 # }
 
-def happy_birthday(birthday_kids)
-  
-end
+def h
+  def happy_birthday(birthday_kids)
+    birthday_kids.each do |kids_name, age|
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    end
+  end
